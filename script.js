e3870c6ba38ac5904793d71c4bb135d4699fd223
afriyiedmanu <afriyiedwobengmanu@gmail.com>
2025-06-24 22:00:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function resizeImage() {
-    const image = document.getElementById("jason");
-    image.classList.toggle("large");
+const jason = document.getElementById('jason');
+    let isEnlarged = false;
+function toggleSizeAndBorder() {
+  if (!isEnlarged) {
+    jason.style.width = '1000px';
+    jason.style.height = '800px';
+    jason.style.border = '10px solid red';
+    jason.style.textAlign='center';
+  } else {
+    jason.style.width = '300px';  
+    jason.style.height = '300px';        
+    jason.style.border = '10px solid blue';
+    jason.style.textAlign='center'          
   }
+  isEnlarged = !isEnlarged;
+}
+jason.addEventListener('click', toggleSizeAndBorder);
